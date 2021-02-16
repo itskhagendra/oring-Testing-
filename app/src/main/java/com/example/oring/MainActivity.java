90fb5b9dbba10096487b608d7b0270271e167e9d
Khagendra Kumar <itskhagendra@hotmail.com>
2021-02-16 14:02:02 +0530

    Image Selection Working
    Gallery Image Selection Working

--- app/src/main/java/com/example/oring/MainActivity.java
+++ app/src/main/java/com/example/oring/MainActivity.java
@@ -145,6 +145,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,-1);
 
     }
 
